93c2b632234970ec263ea79a6c2e613486bb37b6
irb <jose_fernando159@hotmail.com>
2019-08-28 08:29:38 -0700

    Sonido intento notas

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 alert('Wht u want? i´m busy');
+const buttons = document.querySelectorAll('button');
 
-let botonDo = document.getElementById('botonDo')
+let botonDo = document.getElementById('botonDo');
+const playnote =() =>{
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note};`;
 
-botonDo.addEventListener('click', ()=> {alert('Yolo')})
+botonDo.addEventListener('click', ()=> {alert('Yolo')});
+console.log(audioId);
+const audio = document.getElementById(audioId);
+console.log(audio);
+audio.onpause();
+audio.currentTime = 0;
+audio.play();
+
+}
+
+buttons.forEach(
+    button =>
+    button.addEventListener('click', playnote));
